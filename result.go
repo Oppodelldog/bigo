43ddaac8211fbdf49bf9e4fd4d49196100424394
Oppodelldog <odog@netcologne.de>
2019-09-22 13:05:44 +0200

    shorten json output

--- result.go
+++ result.go
@@ -5,11 +5,11 @@ type OMeasure struct {
 	// ResultValue may be used to store result values of the tested logic.
 	// It is not used by this library, it's intention is debugging:
 	// If those are deterministic they could be used to cross check that the tested code worked as expected.
-	ResultValue interface{}
+	ResultValue interface{} `json:"V"`
 	// O represents the number of operations used by the tested program.
 	// Determining this value could be from tricky to impossible.
 	// For a per machine comparison this could also be the duration of the logic under test.
-	O float64
+	O float64 `json:"O"`
 }
 
 // OMeasures contains multiple instances of OMeasure
@@ -17,8 +17,8 @@ type OMeasures []OMeasure
 
 // Result is used by the library to accumulate OMeasure results per N
 type Result struct {
-	N         float64
-	OMeasures OMeasures
+	N         float64   `json:"N"`
+	OMeasures OMeasures `json:"M"`
 }
 
 // Results contains multiple instances of Result
